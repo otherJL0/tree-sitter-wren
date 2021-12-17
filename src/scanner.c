890859375496cceb00befd579d9e75e3afc2766b
otherJL0 <jonathanglopez@gmail.com>
2021-12-16 21:20:59 -0500

    Reformat scanner file

--- src/scanner.c
+++ src/scanner.c
@@ -6,26 +6,12 @@ enum TokenType {
   NEWLINE,
 };
 
-void *tree_sitter_wren_external_scanner_create() {
-  // ...
-}
-
-void tree_sitter_wren_external_scanner_destroy(void *payload) {
-  // ...
-}
-
+void *tree_sitter_wren_external_scanner_create() { return NULL }
+void tree_sitter_wren_external_scanner_destroy(void *payload) {}
 unsigned tree_sitter_wren_external_scanner_serialize(void *payload,
-                                                     char *buffer) {
-  // ...
-}
-
+                                                     char *buffer) {}
 void tree_sitter_wren_external_scanner_deserialize(void *payload,
                                                    const char *buffer,
-                                                   unsigned length) {
-  // ...
-}
-
+                                                   unsigned length) {}
 bool tree_sitter_wren_external_scanner_scan(void *payload, TSLexer *lexer,
-                                            const bool *valid_symbols) {
-  // ...
-}
+                                            const bool *valid_symbols) {}

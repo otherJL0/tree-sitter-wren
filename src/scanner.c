fbf6baf653d5eeec7391c5438eeff530074ccdba
otherJL0 <jonathanglopez@gmail.com>
2021-12-19 21:15:09 -0500

    Update clang-format

--- src/scanner.c
+++ src/scanner.c
@@ -6,12 +6,22 @@ enum TokenType {
   NEWLINE,
 };
 
-void *tree_sitter_wren_external_scanner_create() { return NULL }
+void *tree_sitter_wren_external_scanner_create() { return NULL; }
 void tree_sitter_wren_external_scanner_destroy(void *payload) {}
-unsigned tree_sitter_wren_external_scanner_serialize(void *payload,
-                                                     char *buffer) {}
-void tree_sitter_wren_external_scanner_deserialize(void *payload,
-                                                   const char *buffer,
-                                                   unsigned length) {}
-bool tree_sitter_wren_external_scanner_scan(void *payload, TSLexer *lexer,
-                                            const bool *valid_symbols) {}
+unsigned tree_sitter_wren_external_scanner_serialize(void *payload, char *buffer)
+{
+  return EXIT_SUCCESS;
+}
+void tree_sitter_wren_external_scanner_deserialize(
+    void *payload,
+    const char *buffer,
+    unsigned length)
+{
+}
+bool tree_sitter_wren_external_scanner_scan(
+    void *payload,
+    TSLexer *lexer,
+    const bool *valid_symbols)
+{
+  return EXIT_SUCCESS;
+}
